c3c83fa21e58735756849a3acba5265a5d4b5f3b
Gaurav <gauravkushwaha990111@gmail.com>
2026-03-31 21:26:28 +0530

    Implement file listing in mounted bucket
    Added functionality to list files in a mounted bucket path.

--- startbot.sh
+++ startbot.sh
@@ -3,6 +3,19 @@
 echo "TG to Num Bot starting..."
 ls
 
+
+# Bucket ka mounted path
+BUCKET_PATH="/mnt/data"
+
+echo "Listing all files in bucket: $BUCKET_PATH"
+echo "-----------------------------------------"
+
+# Recursive list
+find "$BUCKET_PATH" -type f
+
+echo "-----------------------------------------"
+echo "Done!"
+
 while true
 do
     echo "[`date`] Starting bot..."
